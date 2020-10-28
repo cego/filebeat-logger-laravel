e588de97286a30f9ecb80ca93d7df3bb05338ae9
Nikolaj Boel Jensen <nbj@cego.dk>
2020-10-28 09:37:28 +0100

    Initial commit

---
+++ src/FilebeatLogging/FilebeatLoggingServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Cego\FilebeatLogging;
+
+use Cego\FilebeatLoggerFactory;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+
+class FilebeatLoggingServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Config::set('logging.filebeat', [
+            'driver'  => 'custom',
+            'channel' => sprintf('%s - %s', env('APP_NAME'), env('APP_ENV')),
+            'via'     => FilebeatLoggerFactory::class
+        ]);
+    }
+}
