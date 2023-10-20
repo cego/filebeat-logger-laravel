90fd8e109ad043403568406a5e963b9851b73a22
Lau Josefsen <lejo@cego.dk>
2023-10-20 13:49:34 +0200

    Detect octane and use process handler for logger

--- src/FilebeatLogging/FilebeatLoggingServiceProvider.php
+++ src/FilebeatLogging/FilebeatLoggingServiceProvider.php
@@ -4,6 +4,7 @@ namespace Cego\FilebeatLogging;
 
 use JsonException;
 use Cego\FilebeatLoggerFactory;
+use Monolog\Handler\ProcessHandler;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Debug\ExceptionHandler;
@@ -19,16 +20,31 @@ class FilebeatLoggingServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        Config::set('logging.channels.filebeat', [
+        $config = [
             'driver'   => 'custom',
             'channel'  => 'filebeat',
             'extras'   => json_decode(env('FILEBEAT_LOGGER_EXTRAS', '{}'), $assoc = true, $depth = 512, JSON_THROW_ON_ERROR),
             'stream'   => env('FILEBEAT_LOGGER_STREAM', 'php://stdout'),
             'rotating' => env('FILEBEAT_LOGGER_ROTATING', false),
             'via'      => FilebeatLoggerFactory::class,
-        ]);
+        ];
+
+        if($this->isOctane()) {
+            $config['handler'] = ProcessHandler::class;
+            $config['with'] = [
+                'command' => 'cat >> /proc/1/fd/1',
+            ];
+        }
+
+        Config::set('logging.channels.filebeat', $config);
 
         /* @phpstan-ignore-next-line */
         $this->app->bind(ExceptionHandler::class, LoggerExceptionHandler::class);
+    }
+
+    /** Detect if application is running octane */
+    protected function isOctane(): bool
+    {
+        return isset($_SERVER['LARAVEL_OCTANE']) && ((int)$_SERVER['LARAVEL_OCTANE'] === 1);
     }
 }
